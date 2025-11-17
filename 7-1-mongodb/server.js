4cf3a83a2067cce0f4609d2414831b9b480b55ef
m25ys <myalsuhaibani@gmail.com>
2025-11-17 23:04:55 +0300

    finished

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -169,23 +169,38 @@
 
 import mongoose from "mongoose";
 
-// establish connection
 
+const connectionString ="mongodb+srv://myalsuhaibani_db_user:McM0VfCQgkv26e5X@cluster0.xiwkvmd.mongodb.net/";
 
 // define schema
-
-
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String,
+});
+const Student = mongoose.model("Student", studentSchema);
 // create document
-
-
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" },
+  ]);
+  console.log("✅ Inserted");
+}
 // read document
-
-
+async function readStudents() {
+  const all = await Student.find();
+  console.log(all);
+}
 // update document
-
-
+async function updateStudent() {
+  await Student.updateOne({ name: "Ali" }, { age: 22 });
+  console.log("✅ Updated Ali");
+}
 // delete document
-
-
+async function deleteStudent() {
+  await Student.deleteOne({ name: "Sara" });
+  console.log("✅ Deleted Sara");
+}
 
 
